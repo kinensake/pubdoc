ba1aec01cb707caa87fa43dfa0952c810d544e37
kinensake <kinensake@gmail.com>
2025-03-20 15:04:38 +0700

    feat: remove .docusaurus

--- internal/docusaurus/project/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/quan.dinh/Desktop/workspace/pubdoc/internal/docusaurus/project/node_modules/.pnpm/infima@0.2.0-alpha.43/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/quan.dinh/Desktop/workspace/pubdoc/internal/docusaurus/project/node_modules/.pnpm/@docusaurus+theme-classic@3.0.0_@types+react@19.0.12_acorn@8.14.1_react-dom@18.3.1_react@18.3.1_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/quan.dinh/Desktop/workspace/pubdoc/internal/docusaurus/project/node_modules/.pnpm/@docusaurus+theme-classic@3.0.0_@types+react@19.0.12_acorn@8.14.1_react-dom@18.3.1_react@18.3.1_typescript@5.2.2/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/quan.dinh/Desktop/workspace/pubdoc/internal/docusaurus/project/src/css/custom.css"),
-];
